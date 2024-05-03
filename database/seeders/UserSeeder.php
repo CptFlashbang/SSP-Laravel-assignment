155e856933cc740421ea6cc82d0696abeed560e7
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-05-03 14:48:48 +0100

    populate user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +12,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'Glenn Treafuker',
+                'email' => 'glenn.treafuker@gmail.com',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Emily Wokerson',
+                'email' => 'emily.wokerson@gmail.com',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Owen Line',
+                'email' => 'owen.line@gmail.com',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
