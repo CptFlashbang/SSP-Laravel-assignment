9b79e38da51bb139a139a56226b5c4fbe1147e88
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-05-03 13:09:02 +0100

    add order and topping relation to pizza model

--- app/Models/Pizza.php
+++ app/Models/Pizza.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Pizza extends Model
 {
     use HasFactory;
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+    public function toppings(): HasMany
+    {
+        return $this->hasMany(Topping::class);
+    }
 }
