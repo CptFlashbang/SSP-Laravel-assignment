d884c21557aa2286ae13ec190fe938e9cd9ad37f
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-05-03 15:21:34 +0100

    populate toppings seeder

--- database/seeders/ToppingSeeder.php
+++ database/seeders/ToppingSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ToppingSeeder extends Seeder
 {
@@ -12,6 +12,35 @@ class ToppingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $toppings = [
+            'Bacon',
+            'Cheese',
+            'Chicken',
+            'Green peppers',
+            'Ham',
+            'Hot dog pieces',
+            'Jalapeno peppers',
+            'Minced beef',
+            'Mushrooms',
+            'Olives',
+            'Onions',
+            'Pepperoni',
+            'Pineapple',
+            'Salami',
+            'Sausage',
+            'Spicy beef',
+            'Sweetcorn',
+            'Tomato sauce',
+            'Vegan cheese'
+        ];
+
+        foreach ($toppings as $topping) {
+            DB::table('toppings')->insert([
+                'name' => $topping,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
+
