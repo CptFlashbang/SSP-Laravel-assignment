af360ddb1418962d29c73d064b3e6e89e79c3a01
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-05-03 15:56:06 +0100

    populate PizzaTopping seeder

--- database/seeders/PizzaToppingSeeder.php
+++ database/seeders/PizzaToppingSeeder.php
@@ -12,6 +12,26 @@ class Pizza_ToppingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Define pizza to toppings mappings
+        $pizzas = [
+            'Margherita' => ['Cheese', 'Tomato sauce'],
+            'Gimme the Meat' => ['Pepperoni', 'Ham', 'Chicken', 'Minced beef', 'Sausage', 'Bacon'],
+            'Veggie Delight' => ['Onions', 'Green peppers', 'Mushrooms', 'Sweetcorn'],
+            'Make Mine Hot' => ['Chicken', 'Onions', 'Green peppers', 'Jalapeno peppers']
+        ];
+
+        // Populate the pizza_topping table
+        foreach ($pizzas as $pizzaName => $toppings) {
+            $pizzaId = DB::table('pizzas')->where('name', $pizzaName)->value('id');
+
+            foreach ($toppings as $toppingName) {
+                $toppingId = DB::table('toppings')->where('name', $toppingName)->value('id');
+
+                DB::table('pizza_topping')->insert([
+                    'pizza_id' => $pizzaId,
+                    'topping_id' => $toppingId
+                ]);
+            }
+        }
     }
 }
