d77d9516c1e4357126128ed61ad61514d48aea99
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-05-03 13:47:24 +0100

    add pizza_id to order table

--- database/migrations/2024_05_03_111220_create_orders_table.php
+++ database/migrations/2024_05_03_111220_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('pizza_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
