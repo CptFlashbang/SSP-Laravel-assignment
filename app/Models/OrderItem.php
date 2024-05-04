379e189e98d93d90f2ddcdbf7db98fad6014cca9
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-05-04 15:32:04 +0100

    Add relationships to OrderItem

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_id', 'pizza_id', 'quantity'];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function pizza(): BelongsTo
+    {
+        return $this->belongsTo(Pizza::class);
+    }
 }
