fa2c2b0d0996cc580f8e88551a8691b01035fab2
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-05-04 23:28:13 +0100

    add price to fillable fields

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -10,7 +10,7 @@ class OrderItem extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['order_id', 'pizza_id', 'quantity', 'size'];
+    protected $fillable = ['order_id', 'pizza_id', 'quantity', 'size', 'price'];
 
     public function order(): BelongsTo
     {
